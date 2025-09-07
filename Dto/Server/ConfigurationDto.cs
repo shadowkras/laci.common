480f9e8d692ba761ae98fd5aea10e4f88f92e92f
Syrilai <30000694+Syrilai@users.noreply.github.com>
2025-09-07 13:32:47 +0200

    rewrite: use new namespaces

---
+++ Dto/Server/ConfigurationDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaciSynchroni.Common.Dto.Server;
+public record ConfigurationDto
+{
+    public string ServerName { get; set; } = string.Empty;
+    public Version ServerVersion { get; set; } = new(0, 0, 0);
+    public Uri ServerUri { get; set; } = new("wss://noemptyuri");
+    public string? DiscordInvite { get; set; } = null;
+    public string? ServerRules { get; set; } = null;
+}
