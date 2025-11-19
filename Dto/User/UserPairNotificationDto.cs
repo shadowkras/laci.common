a3c4cce7c7b1ced3eea8b89e762bfbaecfbcd053
Shadow Kras <shadow_kras@hotmail.com>
2025-11-18 21:48:26 -0400

    Added UserPairNotificationDto

---
+++ Dto/User/UserPairNotificationDto.cs
@@ -0,0 +1,11 @@
+﻿using MessagePack;
+
+namespace LaciSynchroni.Common.Dto.User
+{
+    [MessagePackObject(keyAsPropertyName: true)]
+    public sealed class UserPairNotificationDto
+    {
+        public required string MyHashedCid { get; init; }
+        public required string Message { get; init; }
+    }
+}
